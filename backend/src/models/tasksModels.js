f743b6bb025e117c0234d1a63a2397cd425c577c
Vibes <lucianocardoso21@outlook.com>
2025-02-09 12:07:24 -0300

    atualização 09/02/25
    todas as funções da tabela cliente

--- backend/src/models/tasksModels.js
+++ backend/src/models/tasksModels.js
@@ -1,10 +1,75 @@
 const connection = require('./connection');
 
-const buscarCliente = async ()=>{
-    const tarefa = await connection.execute('SELECT * FROM cliente WHERE cpf=?;');
-    return tarefa;
+const cadastrarCliente = async (cpf, senha, nome, celular, endereco)=>{
+    const query = await connection.execute(
+        'INSERT INTO clientes(cpf, senha, nome, celular, endereco, data_entrada) VALUES (?, ?, ?, ?, ?, NOW());',
+        [cpf, senha, nome, celular, endereco]
+    );
+    return query;
+};
+const buscarClienteCpf = async (cpf)=>{
+    const query = await connection.execute(
+        'SELECT * FROM clientes WHERE cpf = ?;',
+        [cpf]
+    );
+    return query;
+};
+const buscarClienteNome = async(nome)=>{
+    const query = await connection.execute(
+        'SELECT * FROM clientes WHERE nome LIKE ?;',
+        [`%${nome}%`]
+    );
+    return query;
+};
+const atualizarCliente = async (cpf, senha, nome, celular, endereco)=>{
+    const query = await connection.execute(
+        'UPDATE clientes SET senha = ?, nome = ?, celular = ?, endereco = ? WHERE cpf = ?;',
+        [senha, nome, celular, endereco, cpf]
+    );
+    return query;
+};
+const excluirCliente = async(cpf)=>{
+    const query = await connection.execute(
+        'DELETE FROM clientes WHERE cpf = ?;',
+        [cpf]
+    );
+    return query;
+};
+const listarClientes = async ()=>{
+    const query = await connection.execute(
+        'SELECT * FROM clientes;'
+    );
+    return query;
+};
+const verificarClienteExistente = async (cpf)=>{
+    const query = await connection.execute(
+        'SELECT * FROM clientes WHERE cpf = ?;',
+        [cpf]
+    );
+    return query;
+};
+const atualizarCampoCliente = async(cpf, campo, valor)=>{
+    const query = await connection.execute(
+        `UPDATE clientes SET ${campo} = ? WHERE cpf = ?;`,
+        [valor,cpf]
+    );
+    return query;
+};
+const contarClientes = async()=>{
+    const query = await connection.execute(
+        'SELECT COUNT(*) AS total FROM clientes;'
+    );
+    return query;
 };
 
 module.exports = {
-    buscarCliente
+    cadastrarCliente,
+    buscarClienteCpf,
+    atualizarCliente,
+    excluirCliente,
+    listarClientes,
+    verificarClienteExistente,
+    atualizarCampoCliente,
+    buscarClienteNome,
+    contarClientes
 };
